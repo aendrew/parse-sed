c6b9743ae71deecc082482d37c6787c3b3246906
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-06-16 13:19:12 +0100

    Adds some tests and CircleCI support

--- parse-sed.js
+++ parse-sed.js
@@ -1,176 +1,183 @@
+/**
+ * @file
+ * parse-sed.js
+ * 2017 Ændrew Rininsland (@aendrew)
+ *
+ * Forked from sed.js, 2013 David Jones (@drj11)
+ */
+
 const posixbre = require('posixbre');
 
-const intify = (s) => {
-  const x = Number(s);
-  if (!isNaN(x)) {
-    return parseInt(x, 10);
-  }
-  return s;
+const intify = s => {
+	const x = Number(s);
+	if (!isNaN(x)) {
+		return parseInt(x, 10);
+	}
+	return s;
 };
 
-const unEscape1 = (x) => {
-  if (x[0] === '\\') {
-    return x[1];
-  }
-  return x;
+const unEscape1 = x => {
+	if (x[0] === '\\') {
+		return x[1];
+	}
+	return x;
 };
 
 const unEscape = s => s.replace(/[^\\]|\\[\s\S]/g, unEscape1);
 
 const trueNewline = s => s.replace(/[^\\]|\\\n/g, unEscape1);
 
-const asAddr = (s) => {
-  let delim;
-  let re;
-  let x;
-  if (s === '$') {
-    return s;
-  }
-  if (s === undefined) {
-    return x;
-  }
-  x = intify(s);
-  if (typeof x === 'number') {
-    return x;
-  }
-  const ref = x[0];
-  if (Array.prototype.indexOf.call('\\/', ref) >= 0) {
-    if (x[0] === '/') {
-      re = x.slice(1, -1);
-    } else {
-      delim = x.slice(0, 2);
-      x = x.slice(2, -1);
-      re = x.replace(/[^\\]|\\./g, (c) => {
-        if (c === delim) {
-          return c[1];
-        }
-        return c;
-      });
-    }
-    if (re === '') {
-      return 'empty';
-    }
-    return RegExp(posixbre.asRegExp(re));
-  }
+const asAddr = s => {
+	let delim;
+	let re;
+	let x;
+	if (s === '$') {
+		return s;
+	}
+	if (s === undefined) {
+		return x;
+	}
+	x = intify(s);
+	if (typeof x === 'number') {
+		return x;
+	}
+	const ref = x[0];
+	if (Array.prototype.indexOf.call('\\/', ref) >= 0) {
+		if (x[0] === '/') {
+			re = x.slice(1, -1);
+		} else {
+			delim = x.slice(0, 2);
+			x = x.slice(2, -1);
+			re = x.replace(/[^\\]|\\./g, c => {
+				if (c === delim) {
+					return c[1];
+				}
+				return c;
+			});
+		}
+		if (re === '') {
+			return 'empty';
+		}
+		return RegExp(posixbre.asRegExp(re));
+	}
 
-  return undefined;
+	return undefined;
 };
 
-module.exports = (script) => {
-  let nesting = 0;
+module.exports = script => {
+	let nesting = 0;
 
-  const parse1 = (_s) => {
-    let s = _s;
-    let argre;
-    let digits;
-    let m;
-    const re = /(?:(\d+|\$|(?:\\|(?=\/))(.)(?:(?!\2)(?:[^\\\n]|\\.))*\2)(?:,(\d+|\$|(?:\\|(?=\/))(.)(?:(?!\4)(?:[^\\\n]|\\.))*\4))?)?(\s*!+)?([aci]\\|[bDdGgHhlNnPpqrstwxy#:=}{])/g;
-    m = re.exec(s);
-    if (!m) {
-      return [null, null];
-    }
-    const cmd = {};
-    const addr1 = asAddr(m[1]);
-    const addr2 = asAddr(m[3]);
-    if (addr1 != null) {
-      cmd.addr1 = addr1;
-    }
-    if (addr2 != null) {
-      cmd.addr2 = addr2;
-    }
-    cmd.positive = !m[5];
-    cmd.verb = m[6][0];
-    s = s.slice(re.lastIndex);
-    const ref = cmd.verb;
-    if (Array.prototype.indexOf.call('#', ref) >= 0) {
-      s = s.replace(/^.*?(\n|$)/, '');
-    }
-    if (cmd.verb === '{') {
-      cmd.arg = `\n${nesting}`;
-      cmd.positive = !cmd.positive;
-      nesting += 1;
-    }
-    if (cmd.verb === '}') {
-      nesting -= 1;
-      cmd.arg = `\n${nesting}`;
-    }
-    const ref1 = cmd.verb;
-    if (Array.prototype.indexOf.call('aci', ref1) >= 0) {
-      s = s.replace(/^.*?\n/, '');
-      argre = /((?:[^\n\\]|\\[\s\S])*)\n?/g;
-      m = argre.exec(s);
-      cmd.arg = m[1].replace(/\\[\s\S]/g, x => x[1]);
-      s = s.slice(argre.lastIndex);
-    }
-    const ref2 = cmd.verb;
-    if (Array.prototype.indexOf.call(':btrw', ref2) >= 0) {
-      argre = /[^\S\n]*(.*)\n?/g;
-      m = argre.exec(s);
-      cmd.arg = m[1];
-      const ref3 = cmd.verb;
-      if (Array.prototype.indexOf.call('w', ref3) >= 0) {
-        // cmd.stream = mkWrite(cmd.arg);
-        console.warn(`ref3 is ${ref}`);
-      }
-      s = s.slice(argre.lastIndex);
-    }
-    const ref4 = cmd.verb;
-    if (Array.prototype.indexOf.call('sy', ref4) >= 0) {
-      argre = /([^\\\n])((?:(?!\1)[^\\\n]|\\.)*)\1((?:(?!\1)[^\\\n]|\\[\s\S])*)\1([\dgnpw]*)(?:[^\S\n]+(.+))?/g;
-      m = argre.exec(s);
-      cmd.arg = m.slice(1, 6);
-      cmd.delimiter = m[1];
-      cmd.string1 = unEscape(m[2]);
-      cmd.re = RegExp(posixbre.asRegExp(m[2]), 'g');
-      if (m[2] === '') {
-        cmd.re = null;
-      }
-      cmd.replacement = m[3];
-      cmd.replacement = trueNewline(cmd.replacement);
-      if (cmd.verb === 'y') {
-        cmd.replacement = unEscape(cmd.replacement);
-      }
-      cmd.flags = m[4];
-      digits = cmd.flags.match(/\d+/);
-      if (digits) {
-        cmd.nth = +digits;
-      } else {
-        cmd.nth = +(!(Array.prototype.indexOf.call(cmd.flags, 'g') >= 0));
-      }
+	const parse1 = _s => {
+		let s = _s;
+		let argre;
+		let digits;
+		let m;
+		const re = /(?:(\d+|\$|(?:\\|(?=\/))(.)(?:(?!\2)(?:[^\\\n]|\\.))*\2)(?:,(\d+|\$|(?:\\|(?=\/))(.)(?:(?!\4)(?:[^\\\n]|\\.))*\4))?)?(\s*!+)?([aci]\\|[bDdGgHhlNnPpqrstwxy#:=}{])/g;
+		m = re.exec(s);
+		if (!m) {
+			return [null, null];
+		}
+		const cmd = {};
+		const addr1 = asAddr(m[1]);
+		const addr2 = asAddr(m[3]);
+		if (addr1 !== null) {
+			cmd.addr1 = addr1;
+		}
+		if (addr2 !== null) {
+			cmd.addr2 = addr2;
+		}
+		cmd.positive = !m[5];
+		cmd.verb = m[6][0];
+		s = s.slice(re.lastIndex);
+		const ref = cmd.verb;
+		if (Array.prototype.indexOf.call('#', ref) >= 0) {
+			s = s.replace(/^.*?(\n|$)/, '');
+		}
+		if (cmd.verb === '{') {
+			cmd.arg = `\n${nesting}`;
+			cmd.positive = !cmd.positive;
+			nesting += 1;
+		}
+		if (cmd.verb === '}') {
+			nesting -= 1;
+			cmd.arg = `\n${nesting}`;
+		}
+		const ref1 = cmd.verb;
+		if (Array.prototype.indexOf.call('aci', ref1) >= 0) {
+			s = s.replace(/^.*?\n/, '');
+			argre = /((?:[^\n\\]|\\[\s\S])*)\n?/g;
+			m = argre.exec(s);
+			cmd.arg = m[1].replace(/\\[\s\S]/g, x => x[1]);
+			s = s.slice(argre.lastIndex);
+		}
+		const ref2 = cmd.verb;
+		if (Array.prototype.indexOf.call(':btrw', ref2) >= 0) {
+			argre = /[^\S\n]*(.*)\n?/g;
+			m = argre.exec(s);
+			cmd.arg = m[1];
+			const ref3 = cmd.verb;
+			if (Array.prototype.indexOf.call('w', ref3) >= 0) {
+        // Cmd.stream = mkWrite(cmd.arg);
+				console.warn(`ref3 is ${ref}`);
+			}
+			s = s.slice(argre.lastIndex);
+		}
+		const ref4 = cmd.verb;
+		if (Array.prototype.indexOf.call('sy', ref4) >= 0) {
+			argre = /([^\\\n])((?:(?!\1)[^\\\n]|\\.)*)\1((?:(?!\1)[^\\\n]|\\[\s\S])*)\1([\dgnpw]*)(?:[^\S\n]+(.+))?/g;
+			m = argre.exec(s);
+			cmd.arg = m.slice(1, 6);
+			cmd.delimiter = m[1];
+			cmd.string1 = unEscape(m[2]);
+			cmd.re = RegExp(posixbre.asRegExp(m[2]), 'g');
+			if (m[2] === '') {
+				cmd.re = null;
+			}
+			cmd.replacement = m[3];
+			cmd.replacement = trueNewline(cmd.replacement);
+			if (cmd.verb === 'y') {
+				cmd.replacement = unEscape(cmd.replacement);
+			}
+			cmd.flags = m[4];
+			digits = cmd.flags.match(/\d+/);
+			if (digits) {
+				cmd.nth = Number(digits);
+			} else {
+				cmd.nth = Number(!(Array.prototype.indexOf.call(cmd.flags, 'g') >= 0));
+			}
 
-      s = s.slice(argre.lastIndex);
-    }
-    return [s, cmd];
-  };
+			s = s.slice(argre.lastIndex);
+		}
+		return [s, cmd];
+	};
 
-  const parseScript = (_s) => {
-    let s = _s;
-    let cmd;
-    let ref;
-    let ref1;
-    const label = {};
-    const cmds = [];
-    for (;;) {
-      ref = parse1(s);
-      s = ref[0];
-      cmd = ref[1];
-      if (s === null) {
-        break;
-      }
-      cmds.push(cmd);
-      ref1 = cmd.verb;
-      if (Array.prototype.indexOf.call(':}', ref1) >= 0) {
-        label[cmd.arg] = cmds.length;
-      }
-    }
-    return [cmds, label];
-  };
+	const parseScript = _s => {
+		let s = _s;
+		let cmd;
+		let ref;
+		let ref1;
+		const label = {};
+		const cmds = [];
+		for (;;) {
+			ref = parse1(s);
+			s = ref[0];
+			cmd = ref[1];
+			if (s === null) {
+				break;
+			}
+			cmds.push(cmd);
+			ref1 = cmd.verb;
+			if (Array.prototype.indexOf.call(':}', ref1) >= 0) {
+				label[cmd.arg] = cmds.length;
+			}
+		}
+		return [cmds, label];
+	};
 
-
-  const ref = parseScript(script);
-  const [commands, label] = ref;
-  return {
-    commands,
-    label,
-  };
+	const ref = parseScript(script);
+	const [commands, label] = ref;
+	return {
+		commands,
+		label
+	};
 };
---
+++ test.js
@@ -0,0 +1,225 @@
+/**
+ * @file
+ * Unit tests for parse-sed
+ */
+
+import {readFileSync} from 'fs';
+import test from 'ava';
+import sed from './parse-sed';
+
+test('!', t => {
+	const [command1] = sed('2,3!p').commands;
+	t.deepEqual(command1, {
+		addr1: 2,
+		addr2: 3,
+		positive: false,
+		verb: 'p'
+	}, 'Simple case');
+
+	const [command2] = sed('1,/2/!!p').commands;
+	t.deepEqual(command2, {
+		addr1: 1,
+		addr2: /2/,
+		positive: false,
+		verb: 'p'
+	}, 'Multiple ! are acceptable.');
+});
+
+test('#', t => {
+	const {commands: commands1} = sed('s/2/two/;#s/1/one/');
+	t.deepEqual(commands1, [
+		{
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: 's',
+			arg: [
+				'/', '2', 'two', '', undefined
+			],
+			delimiter: '/',
+			string1: '2',
+			re: /2/g,
+			replacement: 'two',
+			flags: '',
+			nth: 1
+		}, {
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: '#'
+		}
+	], 'Comments are allowed');
+
+	const {commands: commands2} = sed('#n\n2p');
+	t.deepEqual(commands2, [
+		{
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: '#'
+		}, {
+			addr1: 2,
+			addr2: undefined,
+			positive: true,
+			verb: 'p'
+		}
+	], 'An initial comment of #n is equivalent to -n');
+});
+
+test('$', t => {
+	const [command] = sed('$p').commands;
+	t.deepEqual(command, {
+		addr1: '$',
+		addr2: undefined,
+		positive: true,
+		verb: 'p'
+	});
+});
+
+test(':', t => {
+	const result = sed(':lbl');
+	t.deepEqual(result, {
+		commands: [
+			{
+				addr1: undefined,
+				addr2: undefined,
+				positive: true,
+				verb: ':',
+				arg: 'lbl'
+			}
+		],
+		label: {
+			lbl: 1
+		}
+	});
+});
+
+test('=', t => {
+	const [command] = sed('/b/=').commands;
+	t.deepEqual(command, {
+		addr1: /b/,
+		addr2: undefined,
+		positive: true,
+		verb: '='
+	});
+});
+
+test('BRE', t => {
+	const [command] = sed('s/|/a/').commands;
+	t.deepEqual(command, {
+		addr1: undefined,
+		addr2: undefined,
+		positive: true,
+		verb: 's',
+		arg: [
+			'/', '|', 'a', '', undefined
+		],
+		delimiter: '/',
+		string1: '|',
+		re: /\|/g,
+		replacement: 'a',
+		flags: '',
+		nth: 1
+	});
+});
+
+test('H', t => {
+	const {commands} = sed('h;H;g');
+	t.deepEqual(commands, [
+		{
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: 'h'
+		}, {
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: 'H'
+		}, {
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: 'g'
+		}
+	]);
+});
+
+test('a', t => {
+	const [command1] = sed(`a\
+bar
+`).commands; // This statement should not be indented.
+	t.deepEqual(command1, {
+		addr1: undefined,
+		addr2: undefined,
+		positive: true,
+		verb: 'b',
+		arg: 'ar'
+	});
+
+	const {commands: commands2} = sed(`a\
+bar
+a\
+baz
+`); // This statement should not be indented.
+	t.deepEqual(commands2, [
+		{
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: 'b',
+			arg: 'ar'
+		}, {
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: 'b',
+			arg: 'az'
+		}
+	]);
+});
+
+test('add', t => {
+	const fixture = readFileSync(`${__dirname}/fixtures/add.txt`, {encoding: 'utf-8'});
+	const result = sed(fixture);
+	t.truthy(result, 'Can parse a reasonably complex script without throwing');
+});
+
+test('addr', t => {
+	const {commands: commands1} = sed(`2a\
+	bar
+`);
+	t.deepEqual(commands1, [
+		{
+			addr1: undefined,
+			addr2: undefined,
+			positive: true,
+			verb: 'b',
+			arg: 'ar'
+		}
+	], 'Single numeric address');
+});
+test.todo('b');
+test.todo('big');
+test.todo('c');
+test.todo('context');
+test.todo('d');
+test.todo('file');
+test.todo('g');
+test.todo('i');
+test.todo('input');
+test.todo('l');
+test.todo('loop');
+test.todo('n');
+test.todo('p');
+test.todo('q');
+test.todo('r');
+test.todo('s');
+test.todo('simple');
+test.todo('squeeze');
+test.todo('srepl');
+test.todo('t');
+test.todo('w');
+test.todo('x');
+test.todo('y');
+test.todo('{');
