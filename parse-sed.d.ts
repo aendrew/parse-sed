09acdcb6d7f71d0f7648dfac230cd951579e45c9
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-06-20 02:45:36 +0100

    Adding TypeScript def

---
+++ parse-sed.d.ts
@@ -0,0 +1,34 @@
+/**
+ * TypeScript definition for parse-sed
+ */
+
+declare namespace parseSed {
+
+  /**
+   * A single sed instruction
+   */
+  export interface Instruction {
+    addr1: number | undefined;
+    addr2: number | undefined;
+    positive: boolean;
+    verb: string;
+    arg?: Array<string|undefined>;
+    delimiter?: string;
+    string1?: string;
+    re?: RegExp|null;
+    replacement?: string;
+    flags?: string;
+    nth?: number;
+  }
+
+  export interface Expression {
+    commands: parseSed.Instruction[];
+    label: {
+      [label: string]: number;
+    }
+  }
+}
+
+declare function parseSed (instruction: string) : parseSed.Result;
+
+export = parseSed;
